aed3b956e9d667bdbf9dcab423f4948ceccb4ec5
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-03-21 20:51:07 +0530

    Abstraction: Added Abstarction.java
    Added Abstract and Final keyword implementation in Abstraction.java
    
    Signed-off by: Progyan1997

---
+++ OOP Basics/Abstraction/Abstraction.java
@@ -0,0 +1,34 @@
+/*
+    As per dictionary, abstraction is the quality of dealing with ideas rather than events.
+    In Object-oriented programming, abstraction is a process of hiding the implementation details from the user, only the functionality will be provided to the user. In other words, the user will have the information on what the object does instead of how it does it.
+*/
+
+abstract class Person{
+    String name;
+    int birth_year;
+    abstract void printData();
+    final int age(){
+        return (2017 - birth_year);
+    }
+}
+
+class Student extends Person{
+    int roll;
+    Student(String n, int r, int yb){
+        name = n;
+        roll = r;
+        birth_year = yb;
+    }
+    void printData(){
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+        System.out.println("Age: " + age());
+    }
+}
+
+class Test{
+    public static void main(String args[]){
+        Student stud = new Student("Progyan", 14, 1997);
+        stud.printData();
+    }
+}

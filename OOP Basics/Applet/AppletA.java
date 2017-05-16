0f8d88085384bb2a5637947c06854d1d7d0f1021
Pragyan Bhattacharya <bprogyan@gmail.com>
2017-05-16 21:40:40 +0530

    Applet: Modified Applet code
    Signed-Off-By: Progyan Bhattacharya <admin@codeprogyan.me>

--- OOP Basics/Applet/AppletA.java
+++ OOP Basics/Applet/AppletA.java
@@ -2,7 +2,11 @@ import java.awt.*;
 import java.applet.*;
 
 public class AppletA extends Applet {
-	public void draw(Graphics g) {
+	public void paint(Graphics g) {
 		g.drawLine(100, 100, 200, 200);
 	}
 }
+
+/*
+	<applet code="AppletA.class" height="300" width="300"></applet>
+*/
